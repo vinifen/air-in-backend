09070e1b879648fa3d8292433677bb6d40674947
ViniciusFn <contact.viniciusfn@gmail.com>
2025-01-10 22:37:57 -0300

    📝 Refresh token logic updated and public token id with uuid

--- src/controller/UserControl.ts
+++ src/controller/UserControl.ts
@@ -3,15 +3,16 @@ import UsersModel from "../model/UsersModel";
 import JWTSessionRefreshService from "../services/JWTSessionRefreshService";
 import bcrypt from "bcrypt";
 import { saltRounds } from "../utils/saltRounds";
+import { uuidv7 } from "uuidv7";
 
 export default class UserControl {
 
   constructor(private modelUser: UsersModel, private jwtSessionRefresh: JWTSessionRefreshService) {}
 
-  async getAllUsers() {
-    const result = await this.modelUser.selectAllUsers();
-    return result;
-  }
+  // async getAllUsers() {
+  //   const result = await this.modelUser.selectAllUsers();
+  //   return result;
+  // }
 
   async getUser(token: string){
     const paylod = this.jwtSessionRefresh.getSessionTokenPayload(token);
@@ -37,16 +38,18 @@ export default class UserControl {
 
     if(insertResponse?.status == false){
       console.log(insertResponse);
-      return {status: insertResponse.status, message: insertResponse.message};
+      return {status: insertResponse.status, message: "Register failed"};
     }
 
     const response = await this.modelUser.selectUserByUsername(username);
     
     if (response) {
       console.log(username + "asdfasd");
-      const paylod: JwtPayload = { userID: response.userID, username: response.username };  
+
+      const publicTokenID = uuidv7();
+      const paylod: JwtPayload = { userID: response.userID, username: response.username, publicTokenID: publicTokenID};  
       
-      const sessionToken: string = this.jwtSessionRefresh.generateSessionToken(paylod);
+      const sessionToken: string = await this.jwtSessionRefresh.generateSessionToken(paylod);
       const refreshToken: string = await this.jwtSessionRefresh.generateRefreshToken(paylod);
 
       return {
@@ -57,7 +60,7 @@ export default class UserControl {
       }
 
     } else {
-      throw new Error('User not found');
+      return { status: false, message: `User not found` };
     }
   }
 }
